350fe504a7c9bfc6231f4c6c1743994de16c9427
Maks Verver <maks@verver.ch>
2024-07-17 14:17:42 +0200

    Prepare release v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,14 +11,15 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
